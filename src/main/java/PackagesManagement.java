cf4294d8f232dd874f8e5e23914c3724d13f2f0b
overkill3314 <84456552+overkill3314@users.noreply.github.com>
2022-02-24 18:58:37 +0100

    Add files via upload

--- src/main/java/PackagesManagement.java
+++ src/main/java/PackagesManagement.java
@@ -1,10 +1,16 @@
 
-
+import hoppin.MySQLConnect;
+import hoppin.Package;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Servlet implementation class PackagesManagement
@@ -25,7 +31,29 @@ public class PackagesManagement extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		MySQLConnect db = new MySQLConnect();
+		int i=0;
+		Cookie [] cookies = request.getCookies(); 
+
+		//Cookie sempre diverso da null, altrimenti AuthFilter se ne accorge e rimanda ad Authentication
+
+		for (Cookie aCookie : cookies) {
+			String name = aCookie.getName();
+			if (name.equals("id")){
+				String value = aCookie.getValue();
+				i = Integer.valueOf(value);
+			}
+		}
+		HttpSession session = request.getSession();
+		
+		ArrayList<Package> PackageList = db.GetPackageList(i);
+		
+		session.setAttribute("PackageList", PackageList);
+		
+		db.disconnect();
+		
+		response.sendRedirect("/hoppin/PackagesManagement.jsp");
+		
 	}
 
 	/**
@@ -33,7 +61,33 @@ public class PackagesManagement extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String cap = request.getParameter("ConfirmAddPackage");
+		if (cap != null) {
+			String Npack = request.getParameter("NPack");
+			String DPack = request.getParameter("DPack");
+			int PPack = Integer.valueOf(request.getParameter("PPack"));
+			
+			int i=0;
+			Cookie [] cookies = request.getCookies(); 
+
+			//Cookie sempre diverso da null, altrimenti AuthFilter se ne accorge e rimanda ad Authentication
+
+			for (Cookie aCookie : cookies) {
+				String name = aCookie.getName();
+				if (name.equals("id")){
+					String value = aCookie.getValue();
+					i = Integer.valueOf(value);
+				}
+			}
+			
+			MySQLConnect db = new MySQLConnect();
+			db.AddPackage(i, Npack, DPack, PPack);
+			
+			doGet(request, response);
+			}
 	}
+	
+	
+	
 
 }
