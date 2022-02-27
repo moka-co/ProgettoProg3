8a078b89b064dc2dbc68242f450b4a681f65e4c4
Kurushh <54906285+Kurushh@users.noreply.github.com>
2022-02-27 18:33:48 +0100

    Aggiunti metodi
    Aggiunti metodi  getPriceList, addElementToPriceList, editPriceList, deleteElementToPriceList,  getSeason, addEtoSeason, removeEtoSeason, editEtoSeason

--- src/main/java/hoppin/MySQLConnect.java
+++ src/main/java/hoppin/MySQLConnect.java
@@ -569,6 +569,327 @@ public class MySQLConnect { //Forse questo dovrebbe diventare Singleton
        
         return al;
     }
+
+
+	public ArrayList<Integer> getTotProfit(int id) {
+		PreparedStatement ps;
+		//ResultSet rs;
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		
+		try {
+			//Check Account Type
+			//String AccType = "";
+			String HotelName ="";
+			ResultSet rs;
+			
+			
+			// Get Hotel Name da rimuovere e testare
+			ps = conn.prepareStatement("select Name from Hotel where OwnerId = ?");
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			rs.next();
+			HotelName = rs.getString("Name");
+			//____________________________________________________________________________________-
+			
+			//la riga 420 va bene
+			HotelName = this.getHotelNameById(id);
+			
+			
+			//Query per prendere la somma del prezzo totale di ogni prenotazione e il numero di prenotazioni
+			ps = conn.prepareStatement(" select sum(PriceList.Price) as sumPrice, count(Reservation.id) as totCount"
+					+ "	from Reservation"
+					+ "		INNER JOIN Room ON Room.Num = Reservation.RoomNum"
+					+ "		INNER JOIN PriceList ON Room.Type=PriceList.RoomType"
+					+ "		WHERE Reservation.HotelName= ? ;"
+					);
+			ps.setString(1, HotelName);
+			rs = ps.executeQuery();
+			rs.next();
+			int sumPrice = rs.getInt("sumPrice");
+			int totCount = rs.getInt("totCount");
+			
+			//Aggiungi gli elementi trovati dalla Query all'array
+			res.add(sumPrice);
+			res.add(totCount);
+			
+			
+		}catch ( SQLException e) {
+			System.out.println(e);
+			res.add(-1); res.add(-1);
+		}
+		
+		return res;
+	}
+	
+	public String getAccTypeById(int id) { //Questo metodo è richiamato da più metodi di questa classe
+		String AccType="";
+		
+		try {
+		PreparedStatement ps = conn.prepareStatement("select accType from User where id = ?");
+		ps.setInt(1, id);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		if (rs.getString("accType") != null) {
+			AccType = rs.getString("accType");
+		}
+		
+		} catch  ( SQLException e) {
+			System.out.println(e);
+		}
+		
+		return AccType;
+	}
+	
+	
+	public String getHotelNameById(int id) { //Questo metodo è richiamato da più metodi di questa classe
+		String HotelName = "";
+		try {
+			String AccType = this.getAccTypeById(id);
+			if ( AccType.equals("Owner")) {
+				PreparedStatement ps = conn.prepareStatement("select Name from Hotel where OwnerId = ?");
+				ps.setInt(1, id);
+				ResultSet rs = ps.executeQuery();
+				rs.next();
+				HotelName = rs.getString("Name");
+				
+			}else if (AccType.equals("Employee")) {
+				PreparedStatement ps = conn.prepareStatement("select Name from Hotel where OwnerId = (select sid from User where id = ?) ");
+				ps.setInt(1, id);
+				ResultSet rs = ps.executeQuery();
+				rs.next();
+				HotelName = rs.getString("Name");
+			}
+			
+		}catch (SQLException e) {
+			System.out.println(e);
+		}
+		
+		return HotelName;
+	}
+	
+	
+	public ArrayList<PriceList> getPriceList(int id){ //PriceList = prezziario
+		ArrayList<PriceList> al = new ArrayList<PriceList>(); //al = array list
+		
+		try {
+			String HotelName = this.getHotelNameById(id);
+			PreparedStatement ps = conn.prepareStatement("select RoomType, Price from PriceList where Hotel = ?");
+			ps.setString(1, HotelName);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				String RoomType = rs.getString("RoomType");
+				int price = rs.getInt("Price");
+				PriceList pl = new PriceList(RoomType,price);
+				al.add(pl);
+			}
+			
+		} catch ( SQLException e) {
+			System.out.println(e);
+			
+		}
+		
+		return al;
+	}
+	
+	public boolean addElementToPriceList(String RoomType, int price, int id) {
+		try {
+			String Hotel = this.getHotelNameById(id);
+			PreparedStatement ps = conn.prepareStatement("insert into PriceList (Price, RoomType, Hotel ) values ( ?, ?, ? )");
+			ps.setInt(1, price);
+			ps.setString(2, RoomType);
+			ps.setString(3, Hotel);
+			ps.execute();
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean editPriceList(String RoomType, int price, int id) {
+		
+		try {
+			String Hotel = this.getHotelNameById(id);
+			PreparedStatement ps = conn.prepareStatement("update PriceList set Price = ? where RoomType = ? and Hotel = ?");
+			ps.setInt(1, price);
+			ps.setString(2, RoomType);
+			ps.setString(3, Hotel);
+			ps.execute();
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+			return false;
+		}
+		return true;
+		
+	}
+	
+	public boolean deleteElementToPriceList(String RoomType, int id) {
+		boolean res = true;
+		try {
+			String Hotel = this.getHotelNameById(id);
+			PreparedStatement ps = conn.prepareStatement("delete from PriceList where Hotel = ? and RoomType = ?");
+			ps.setString(1, Hotel);
+			ps.setString(2, RoomType);
+			ps.execute();
+			
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+			return false;
+		}
+		return res;
+	}
+	
+	public ArrayList<Season> getSeason(int id){ //PriceList = prezziario
+		ArrayList<Season> al = new ArrayList<Season>(); //al = array list
+		
+		try {
+			String HotelName = this.getHotelNameById(id);
+			PreparedStatement ps = conn.prepareStatement("select Type, Start, End, PercentIncrease from Season where Hotel = ?");
+			ps.setString(1, HotelName);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				String Type = rs.getString("Type");
+				String Start = rs.getString("Start");
+				String End = rs.getString("End");
+				int price = rs.getInt("PercentIncrease");
+				Season pl = new Season(HotelName, Type, Start, End, price);
+				al.add(pl);
+			}
+			
+		} catch ( SQLException e) {
+			System.out.println(e);
+			
+		}
+		
+		return al;
+	}
+	
+	public boolean addEtoSeason(int id, String season, String startDate, String endDate, int price) {
+		String HotelName = this.getHotelNameById(id);
+		
+		try {
+			
+			PreparedStatement ps = conn.prepareStatement(" INSERT INTO Season (Type, Start, End, PercentIncrease, Hotel)"
+					+ " VALUES ( ?,  ? ,  ? ,  ? , ? ) ");
+			ps.setString(1, season);
+			ps.setString(2, startDate);
+			ps.setString(3, endDate);
+			ps.setInt(4, price);
+			ps.setString(5, HotelName);
+			ps.execute();
+			
+		} catch ( SQLException e) {
+			System.out.println(e);
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
+	public boolean removeEtoSeason(int id, String etoSeason) {
+		String HotelName = this.getHotelNameById(id);
+		String dates [] = etoSeason.split("&");
+		String startDate = dates[0];
+		String endDate = dates[1];
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement("DELETE from Season where Hotel = ? and Start = ? and End = ?");
+			ps.setString(1, HotelName);
+			ps.setString(2, startDate);
+			ps.setString(3, endDate);
+			ps.execute();
+			
+		} catch( SQLException e) {
+			System.out.println(e);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean editEtoSeason(int id, String etoSeason, String season, String startDate, String endDate, int incr) {
+		String HotelName = this.getHotelNameById(id);
+		String dates [] = etoSeason.split("&");
+		String OLDstartDate = dates[0];
+		String OLDendDate = dates[1];
+		
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("UPDATE Season SET");
+			//bitmap in java che controlla se il precedente è 1 allora metti la virgola
+			//Altrimenti non la mettere
+			boolean prec = false;
+			if ( ! season.equals("")) {
+				sb.append("Type = ?");
+				prec = true;
+			}
+			if (! startDate.equals("") ) {
+				if ( prec ) {
+					sb.append(",");
+				}
+				sb.append(" Start = ? ");
+				prec = true;
+			}
+			if (! endDate.equals("") ) {
+				if ( prec ) {
+					sb.append(",");
+				}
+				sb.append(" End = ? ");
+				prec = true;
+			}
+			if (incr != 0) {
+				if ( prec ) {
+					sb.append(",");
+				}
+				sb.append(" PercentIncrease = ?");
+				prec = true;
+			}
+			
+			sb.append(" WHERE Hotel = ? and Start = ? and End = ?");
+			String query = sb.toString();
+			PreparedStatement ps = conn.prepareStatement(query);
+			int i=1;
+			if ( ! season.equals("")) {
+				ps.setString(1, season);
+				i++;
+			}
+			if ( ! startDate.equals("")) {
+				ps.setString(i, startDate);
+				i++;
+			}
+			if ( ! endDate.equals("") ) {
+				ps.setString(i, endDate);
+				i++;
+			}
+			if ( incr != 0) {
+				ps.setInt(i, incr);
+				i++;
+			}
+			ps.setString(i, HotelName);
+			i++;
+			ps.setString(i, OLDstartDate);
+			i++;
+			ps.setString(i, OLDendDate);
+			
+			//un idea per migliorare la leggibilità del codice potrebbe essere
+			//Visto che sto usando degli if per controllare quanti elementi ho
+			//Allora quando vado ad inserire ad esempio Start = ?, sostituisco 
+			//? direttamente con OLDstartDate e vedo se funziona
+			ps.execute();
+			
+		} catch( SQLException e) {
+			System.out.println(e);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
 }
 
 
