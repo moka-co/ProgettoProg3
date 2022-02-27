89c8bbbafe7fc94e4744a69e7d53c86bb1794cc2
Kurushh <54906285+Kurushh@users.noreply.github.com>
2022-02-27 18:21:14 +0100

    Update Authentication.java

--- src/main/java/Authentication.java
+++ src/main/java/Authentication.java
@@ -9,10 +9,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 import hoppin.*;
