316c9ec158714667147a05c7005f087a5c1481ad
Kurushh <54906285+Kurushh@users.noreply.github.com>
2022-03-15 12:31:44 +0100

    Update MySQLConnect.java

--- src/main/java/hoppin/util/sql/MySQLConnect.java
+++ src/main/java/hoppin/util/sql/MySQLConnect.java
@@ -3,8 +3,12 @@ import java.sql.*;
 
 import hoppin.util.factory.PropertyFactory;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 public class MySQLConnect { 
-	protected Connection conn = null;
+	protected MysqlDataSource mysqlDS = null;
+	
+	protected Connection conn;
 	protected String passw;
 	protected String user;
 	protected String dburl;
@@ -16,29 +20,32 @@ public class MySQLConnect {
 			user = PropertyFactory.getInstance().getPropertyMap().get("user");
 			passw = PropertyFactory.getInstance().getPropertyMap().get("password");
 			dburl = PropertyFactory.getInstance().getPropertyMap().get("url");
-	          
-			//Per motivi di troubleshooting
-	        /*
-	        try {
-	        	Class.forName(“com.mysql.jdbc.Driver”);
-	        } catch ( Exception e) {
-	        	System.out.println(e);
-	        } 
 	        
-	        */
-	        
-			conn = DriverManager.getConnection(this.dburl, this.user, this.passw); //Connettiti al database
+	        //Connettiti al database
+			//conn = DriverManager.getConnection(this.dburl, this.user, this.passw); //Connettiti al database
+			mysqlDS = new MysqlDataSource();
+			mysqlDS.setURL(dburl);
+			mysqlDS.setUser(user);
+			mysqlDS.setPassword(passw);
 			
-		} catch (SQLException e) { //Errore se la connessione al database non è corretta
+			conn = mysqlDS.getConnection();
+			
+			
+		} catch (SQLException e) { //Errore se la connessione al database non avviene in modo corretto
 			System.out.println(e);
 			System.out.println("Error while connecting to the database");
+			
+		
 		}
 	}
 	
 	public void disconnect() {
 		
 		try {
-			this.conn.close();
+			if ( conn.isClosed() == false ) { //controlla se non è già chiusa
+				conn.close();
+			}
+
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
